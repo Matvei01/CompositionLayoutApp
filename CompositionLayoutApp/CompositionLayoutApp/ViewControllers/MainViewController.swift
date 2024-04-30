a334889d3d03d88c3ea950602e12642290620747
Matvei <khlestov.matvei@mail.ru>
2024-04-30 13:49:50 +0500

    Created base project, removed storyboard references

---
+++ CompositionLayoutApp/CompositionLayoutApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  CompositionLayoutApp
+//
+//  Created by Matvei Khlestov on 30.04.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
